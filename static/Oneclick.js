6f1f45f01dd746ecc793b4021fe3b811210b7e50
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-08-23 09:29:51 -0300

    Update Oneclick.js

--- static/Oneclick.js
+++ static/Oneclick.js
@@ -4,6 +4,5 @@ function Oneclick(){
 
     function minhaFuncao(evento) {
         console.log("O evento foi acionado pela primeira vez!");
-        // Seu código aqui, que será executado apenas uma vez
     }
 }
